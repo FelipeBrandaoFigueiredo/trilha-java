13fb94a3b52daedff5b7f9b488d0710bb01bec4a
FelpsBrands <felipebrandao2005sp@gmail.com>
2023-09-12 20:26:42 -0300

    Continuação dos meus estudos em Java, agora estudando as estruturas de controle de fluxo, onde criamos outra classe ResultadoEscolarTernario, mas para ilustrar agora, uma condição ternária, atribuida a variável resultado do tipo String, onde determina ao receber a nota do aluno no terminal através da variável nota do tipo inteiro, se o aluno é aprovado, aluno em recuperação ou reprovado.

---
+++ trilha-java/controle-fluxo/src/ResultadoEscolarTernario.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ResultadoEscolarTernario {
+    public static void main (String [] args){
+        Scanner input = new Scanner (System.in);
+        int nota;
+
+        System.out.print("Digite a nota do aluno: ");
+        nota = input.nextInt();
+
+
+        String resultado = nota >= 7 ? "Aluno aprovado" : nota >= 5 && nota < 7 ? "Aluno em recuperação" : "Aluno reprovado";
+
+        System.out.println(resultado);
+
+    }
+}
