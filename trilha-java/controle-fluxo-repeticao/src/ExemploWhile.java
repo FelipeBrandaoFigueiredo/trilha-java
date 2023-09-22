c3db7b1776897f3e6b9893f99e24d1858152d555
FelpsBrands <felipebrandao2005sp@gmail.com>
2023-09-21 22:49:33 -0300

    Olá! Neste novo aprendizado, utilizamos o controle de fluxo de repetição, onde através do novo projeto controle-fluxo-repeticao, criamos classes para aprendermos e vermos a funcionalidade dos controle de fluxo For, While e o Do-While, e vimos também sobre as palavras reservadas Break e Continue, vendo seu comportamento sendo submetido a teste de código.

--- trilha-java/controle-fluxo-repeticao/src/ExemploWhile.java
+++ trilha-java/controle-fluxo-repeticao/src/ExemploWhile.java
@@ -1,28 +1,29 @@
 import java.util.concurrent.ThreadLocalRandom;
+public class ExemploWhile{
 
-public class ExemploWhile {
-    public static void main(String[] args) {
-        double mesada = 50.00;
+    public static void main(String [] args){
+        double mesada = 50.0;
+        double valorDoce = valorAleatorio();
 
-        while (mesada > 0) {
-            double valorDoce = valorAletorio();
+        while(mesada > 0){
 
-            if (valorDoce > mesada) {
+            if(valorDoce > mesada){
                 valorDoce = mesada;
-
             }
             System.out.printf("\nDoce do valor %.2f adicionado no carrinho.", valorDoce);
+
             mesada -= valorDoce;
 
         }
+
         System.out.println("\n\nMesada: " + mesada);
         System.out.println("Joãozinho gastou toda a sua mesada.");
 
+
+    }private static double valorAleatorio(){
+
+        return ThreadLocalRandom.current().nextDouble(2,8);
     }
 
-    private static double valorAletorio() {
-
-        return ThreadLocalRandom.current().nextDouble(2, 8);
-    }
 
 }
